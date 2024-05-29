0d21b5ec5ff764e75b761cc867c83a00c070a9e5
Jenifer Scarlate <jenifer.scarlate@unesp.br>
2024-05-29 16:46:07 -0300

    validando campos das telas de cadastro e login

--- public/script.js
+++ public/script.js
@@ -119,3 +119,118 @@ function atualizarCep(enderecoCompleto) {
 
     })
 }
+
+
+let btn = document.querySelector('.fa-eye');
+let btnVerConfirme = document.querySelector('#verConfirmeSenha');
+
+let nome = document.querySelector('#nome');
+let labelNome = document.querySelector('#labelNome');
+let validNome = false;
+
+let email = document.querySelector('#email');
+let labelEmail = document.querySelector('#labelEmail');
+let validEmail = false;
+
+let senha = document.querySelector('#senha');
+let labelSenha = document.querySelector('#labelSenha');
+let validSenha = false;
+
+let confirmeSenha = document.querySelector('#confirmeSenha');
+let labelConfirmeSenha = document.querySelector('#labelConfirmeSenha');
+let validConfirmeSenha = false;
+
+
+nome.addEventListener('keyup', () => {
+    if (nome.value.length <=  3) {
+        labelNome.setAttribute('style', 'color: red');
+        labelNome.innerHTML = 'Nome *Insira no mínimo 3 caracteres*'
+        nome.setAttribute('style', 'border-color: red');
+        validNome = false;
+    }
+    else {
+        labelNome.setAttribute('style', 'color: green');
+        labelNome.innerHTML = 'Nome'
+        nome.setAttribute('style', 'border-color: green');
+        validNome = true;
+    }
+});
+
+email.addEventListener('keyup', () => {
+    if (email.value.length <=  8) {
+        labelEmail.setAttribute('style', 'color: red');
+        labelEmail.innerHTML = 'E-mail *Insira um e-mail válido*'
+        email.setAttribute('style', 'border-color: red');
+        validEmail = false;
+    }
+    else {
+        labelEmail.setAttribute('style', 'color: green');
+        labelEmail.innerHTML = 'E-mail'
+        email.setAttribute('style', 'border-color: green');
+        validEmail = true;
+    }
+});
+
+senha.addEventListener('keyup', () => {
+    if (senha.value.length <=  5) {
+        labelSenha.setAttribute('style', 'color: red');
+        labelSenha.innerHTML = 'Senha *Insira no mínimo 6 caracteres*'
+        senha.setAttribute('style', 'border-color: red');
+        validSenha = false;
+    }
+    else {
+        labelSenha.setAttribute('style', 'color: green');
+        labelSenha.innerHTML = 'Senha'
+        senha.setAttribute('style', 'border-color: green');
+        validSenha = true;
+    }
+});
+
+
+confirmeSenha.addEventListener('keyup', () => {
+    if (senha.value != confirmeSenha.value) {
+        labelConfirmeSenha.setAttribute('style', 'color: red');
+        labelConfirmeSenha.innerHTML = 'Confirmar senha *As senhas não são conferem*'
+        confirmeSenha.setAttribute('style', 'border-color: red');
+        validConfirmeSenha = false;
+    }
+    else {
+        labelConfirmeSenha.setAttribute('style', 'color: green');
+        labelConfirmeSenha.innerHTML = 'Confirmar senha'
+        confirmeSenha.setAttribute('style', 'border-color: green');
+        validConfirmeSenha = true;
+    }
+});
+
+function cadastrar() {
+    if(validNome && validEmail && validSenha && validConfirmeSenha){
+
+    }
+    else {
+        alert("Preencha todos os campos corretamente");
+    }
+}
+
+btn.addEventListener('click', () => {
+    let inputSenha = document.querySelector('.senha');
+
+    if(inputSenha.getAttribute('type') == 'password') {
+        inputSenha.setAttribute('type', 'text');
+    }
+    else {
+        inputSenha.setAttribute('type', 'password');
+    }
+});
+
+
+btnVerConfirme.addEventListener('click', () => {
+    let inputVerConfirmeSenha = document.querySelector('#confirmeSenha');
+
+    if(inputVerConfirmeSenha.getAttribute('type') == 'password') {
+        inputVerConfirmeSenha.setAttribute('type', 'text');
+    }
+    else {
+        inputVerConfirmeSenha.setAttribute('type', 'password');
+    }
+});
+
